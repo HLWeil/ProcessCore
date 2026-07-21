5cee58a833e9ae70bc2797095938ac8b1edd6168
HLWeil <hl.weil@t-online.de>
2026-07-21 18:12:02 +0200

    start on fable update to reinclude python target

--- build/TestTasks.fs
+++ build/TestTasks.fs
@@ -87,5 +87,5 @@ let runTestsPy = BuildTask.createFn "runTestsPy" [clean] (fun tp ->
 
 let runTests =
     // TODO: add back python tests when FsSpreadsheet
-    BuildTask.create "RunTests" [ clean; buildSolution; runTestsDotnet; (*runTestsPy;*) runTestsJs ] {
+    BuildTask.create "RunTests" [ clean; buildSolution; runTestsDotnet; runTestsPy; runTestsJs ] {
     }
